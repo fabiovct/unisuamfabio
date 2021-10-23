1d550809f65892bb64fe791a7c49f4f270bc141a
fabiovct <vct.fabio.vct@gmail.com>
2021-10-22 21:35:51 -0300

    criado o seeder

--- unisuam-backend/database/seeders/DatabaseSeeder.php
+++ unisuam-backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('status_indicacoes')->insert(array(
+            [
+             'id'=> 1,   
+            'descricao' => 'INICIADA'
+            ],
+        [
+            'id'=> 2, 
+            'descricao' => 'EM PROCESSO'
+        ],
+        [
+            'id'=> 3, 
+            'descricao' => 'FINALIZADA'
+        ]
+        ));
+
+        DB::table('users')->insert(array(
+            [
+            'name'=> 'admin',   
+            'email' => 'admin@admin.com.br',
+            'password' => '123456'
+            ]
+        ));
     }
 }
